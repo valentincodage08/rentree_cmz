542c0e7786d37bc4ea28828e98a8ea44935b810d
valentincodage08 <valentin_mg08@hotmail.fr>
2020-06-29 09:57:29 +0200

    connexion qcm

---
+++ qcm/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row mh-100vh">
+            <div class="col-10 col-sm-8 col-md-6 col-lg-6 offset-1 offset-sm-2 offset-md-3 offset-lg-0 align-self-center d-lg-flex align-items-lg-center align-self-lg-stretch bg-white p-5 rounded rounded-lg-0 my-5 my-lg-0" id="login-block">
+                <div class="m-auto w-lg-75 w-xl-50">
+                    <center><img src="img/logomairieCM.png" class="mb-5" style="height:125px;"></center>
+                    <form action="traitements/connexion.php" method="POST">
+                        <div class="form-group"><label class="text-secondary">Nom d'équipe</label><input class="form-control" name="teamname" type="text" required inputmode="email"></div>
+                        <div class="form-group"><label class="text-secondary">Mot De Passe</label><input class="form-control" name="password" type="password" required></div>
+                        <center><button class="btn btn-info mt-2" type="submit">Se Connecter</button></center>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-6 d-flex align-items-end" id="bg-block" style="background-image:url(&quot;img/gacolor.jpeg&quot;);background-size:cover;background-position:center center;">
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ qcm/traitements/connexion.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include('../../include/connexiondbval.php');
+
+$teamname = $_POST['teamname'];
+$password = $_POST['password'];
+
+$connexion = $bdd->prepare("SELECT team_name, password_manif FROM rdeexpressregister WHERE team_name = :teamname AND password_manif = :password");
+$connexion->execute(array(
+    'teamname' => $teamname,
+    'password' => $password
+));
+
+$resultat = $connexion->fetch();
+$nbresultats = $connexion->rowCount();
+
+if ($nbresultats == 1) {
+
+    $_SESSION['team'] = $resultat['team_name'];
+
+    header('location: ../qcm.php');
+}
+
+
+
+?>

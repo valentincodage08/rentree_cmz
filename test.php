749cb5a86fcb82191f93fb3545b20d42cdcf8bdd
Anthony08240 <beenen@simplon-charleville.fr>
2020-07-02 15:53:23 +0200

    test carte geoloc

--- test.php
+++ test.php
@@ -26,41 +26,66 @@
         integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
         crossorigin=""></script>
     <script type="text/javascript">
+
+
+    function geoloc(){ // ou tout autre nom de fonction
+    var geoSuccess = function(position) { // Ceci s'exécutera si l'utilisateur accepte la géolocalisation
+        startPos = position;
+        userlat = startPos.coords.latitude;
+        userlon = startPos.coords.longitude;
+        console.log("lat: "+userlat+" - lon: "+userlon);
+    };
+    var geoFail = function(){ // Ceci s'exécutera si l'utilisateur refuse la géolocalisation
+        console.log("refus");
+    };
+    // La ligne ci-dessous cherche la position de l'utilisateur et déclenchera la demande d'accord
+    navigator.geolocation.getCurrentPosition(geoSuccess,geoFail);
+}
+
+
         // On initialise la latitude et la longitude de Paris (centre de la carte)
-        var lat = 48.852969;
-        var lon = 2.349903;
+        var lat = 49.772718;
+        var lon = 4.714885;
         var macarte = null;
+        // Nous initialisons une liste de marqueurs
+        var villes = {
+            "Paris": {
+                "lat": 48.852969,
+                "lon": 2.349903
+            },
+            "Brest": {
+                "lat": 48.383,
+                "lon": -4.500
+            },
+            "Quimper": {
+                "lat": 48.000,
+                "lon": -4.100
+            },
+            "Bayonne": {
+                "lat": 43.500,
+                "lon": -1.467
+            }
+        };
         // Fonction d'initialisation de la carte
         function initMap() {
             // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
-            macarte = L.map('map').setView([lat, lon], 11);
+            macarte = L.map('map').setView([lat, lon], 14);
             // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
             L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
                 // Il est toujours bien de laisser le lien vers la source des données
-                attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+                attribution: 'données © OpenStreetMap/ODbL - rendu OSM France',
                 minZoom: 1,
                 maxZoom: 20
             }).addTo(macarte);
+            // Nous parcourons la liste des villes
+            for (ville in villes) {
+                var marker = L.marker([villes[ville].lat, villes[ville].lon]).addTo(macarte);
+            }
         }
         window.onload = function () {
             // Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
             initMap();
         };
-    </script>
-    <script>
-        function geoloc() { // ou tout autre nom de fonction
-            var geoSuccess = function (position) { // Ceci s'exécutera si l'utilisateur accepte la géolocalisation
-                startPos = position;
-                userlat = startPos.coords.latitude;
-                userlon = startPos.coords.longitude;
-                console.log("lat: " + userlat + " - lon: " + userlon);
-            };
-            var geoFail = function () { // Ceci s'exécutera si l'utilisateur refuse la géolocalisation
-                console.log("refus");
-            };
-            // La ligne ci-dessous cherche la position de l'utilisateur et déclenchera la demande d'accord
-            navigator.geolocation.getCurrentPosition(geoSuccess, geoFail);
-        }
     </script>
 </body>
 
